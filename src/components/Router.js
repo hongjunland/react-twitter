4927bafef1f61094a91c8f491d5074cb13abcc4a
이홍준 <zunza96@naver.com>
2022-02-24 02:37:44 +0900

    feat : sign up

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,3 @@
-import React, {useState} from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,45 +1,67 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+} from "firebase/auth";
 
-const Auth = ()=> {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const onChange = (event)=>{
-       const {target: {name, value}} = event;
-       if(name === "email"){
-           setEmail(value)
-       } else if(name === "password"){
-           setPassword(value)
-       }
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const auth = getAuth();
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+        setEmail(value);
+    } else if (name === "password") {
+        setPassword(value);
+    } 
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+        let data = null;
+      if (newAccount) {
+        // create account
+        data = await createUserWithEmailAndPassword(auth, email, password);
+      } else {
+        // log in
+        data = await signInWithEmailAndPassword(auth, email, password);
+      }
+      console.log(data);
+    } catch (error) {
+        console.log(error);
     }
-    const onSubmit = (event) =>{
-        event.preventDefault();
-    }
-    return (
+  };
+  return (
     <div>
-        <form>
-            <input 
-                name="email"
-                type="email"
-                placeholder="Email"
-                required
-                value={email}
-                onChange={onChange}
-            />
-            <input
-                name="password"
-                type="password"
-                placeholder="Password"
-                required
-                value={password}
-                onChange={onChange}
-            />
-            <input type="submit" value="Log in"/>
-        </form>  
-        <div>
-            <button>Continue with Google</button>
-            <button>Continue with Github</button>
-        </div>  
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"}/>
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
     </div>
-    )
-}
+  );
+};
 export default Auth;
